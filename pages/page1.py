962cf097eed1c87e845404e097472b31bc26021f
Frans Grau <fransgrau01@gmail.com>
2023-02-01 16:46:19 +0100

    update

--- pages/page1.py
+++ pages/page1.py
@@ -20,20 +20,37 @@ dropdown_variety = list(map(lambda vrty: str(vrty), wines['variety'].unique()))
 
 ###Static figures 
 winexcountry = wines.groupby('country')['title'].count().sort_values(ascending=False)
-fig1 = px.histogram(winexcountry, x=winexcountry.index, y=winexcountry.values,nbins=50)
+fig1 = px.histogram(winexcountry, x=winexcountry.index, y=winexcountry.values,nbins=50,title="Largest wine producers",labels=dict(x="Country", y="wines"))
 fig1.update_layout(paper_bgcolor = "rgba(0,0,0,0)",
                   plot_bgcolor = "rgba(0,0,0,0)")
 
 grapescore = wines.groupby('variety')['points'].mean().sort_values(ascending=False)
-fig2 = px.bar(grapescore[:5], x=grapescore.index[:5], y=grapescore.values[:5],color_discrete_sequence =['green']*5,title="Best Rated Regions")
+fig2 = px.bar(grapescore[:5], x=grapescore.index[:5], y=grapescore.values[:5],color_discrete_sequence =['green']*5,title="Best Rated Regions",labels=dict(x="", y="Points"))
 fig2.update_layout(paper_bgcolor = "rgba(0,0,0,0)",
                   plot_bgcolor = "rgba(0,0,0,0)")
 
 grapescore2 = wines.groupby('variety')['points'].mean().sort_values(ascending=True)
-fig3 = px.bar(grapescore2[:5], x=grapescore2.index[:5], y=grapescore2.values[:5],color_discrete_sequence =['red']*5,title="Worst Rated Regions")
+fig3 = px.bar(grapescore2[:5], x=grapescore2.index[:5], y=grapescore2.values[:5],color_discrete_sequence =['red']*5,title="Worst Rated Regions",labels=dict(x="", y="Points"))
 fig3.update_layout(paper_bgcolor = "rgba(0,0,0,0)",
                   plot_bgcolor = "rgba(0,0,0,0)")
 
+pxccounts = wines.groupby("province")['country'].count().reset_index().sort_values('country',ascending=False)
+top_7 = pxccounts[:5]
+other = pxccounts[5:].sum()
+other["province"] = "Other"
+pxccounts = top_7.append(other, ignore_index=True)
+fig4 = px.pie(pxccounts, values="country", names="province",hole=.5,title="Largest Producing Regions")
+fig4.update_layout(paper_bgcolor = "rgba(0,0,0,0)",
+                  plot_bgcolor = "rgba(0,0,0,0)")
+
+vxccounts = wines.groupby("variety")['country'].count().reset_index().sort_values('country',ascending=False)
+top_7 = vxccounts[:5]
+other = vxccounts[5:].sum()
+other["variety"] = "Other"
+vxccounts = top_7.append(other, ignore_index=True)
+fig5 = px.pie(vxccounts, values="country", names="variety",hole=.5,title="Most Common Grape Variety")
+fig5.update_layout(paper_bgcolor = "rgba(0,0,0,0)",
+                  plot_bgcolor = "rgba(0,0,0,0)")
 ###Layout
 layout = html.Div(
     [
@@ -63,18 +80,22 @@ layout = html.Div(
     ),
     ],
         ),
-        dbc.Row(
+        html.Br(),
+            dbc.Row(
     [
     dbc.Col(html.Div(
     dcc.Graph(id='fig4',
-              ))
+              figure=fig4
+              )),md=6,
     ),
     dbc.Col(html.Div(
     dcc.Graph(id='fig5',
-              ))
+              figure=fig5,
+              )),md=6,
     ),
     ],
         ),
+
     
     ]
 ),
