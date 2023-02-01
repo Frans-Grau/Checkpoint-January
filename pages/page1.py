8f41d5ed3a5e253a3f1851ce2b0c06719989d9b4
Frans Grau <fransgrau01@gmail.com>
2023-02-01 15:43:16 +0100

    update

--- pages/page1.py
+++ pages/page1.py
@@ -13,9 +13,14 @@ wines = pd.read_csv(link)
 dash.register_page(__name__,path='/', name = 'Airport Analysis') #slash is homepage
 
 ### Dropdown menus
-dropdown_country = list(map(lambda ctry: str(ctry), wines['country'].unique()))
+
+dropdown_country = list(map(lambda ctr: str(ctr), wines['country'].unique()))
 dropdown_province = list(map(lambda provin: str(provin), wines['province'].unique()))
 dropdown_variety = list(map(lambda vrty: str(vrty), wines['variety'].unique()))
+
+###Static figures 
+winexcountry = wines.groupby('country')['title'].count().sort_values(ascending=False)
+fig1 = px.histogram(winexcountry, x=winexcountry.index, y=winexcountry.values,nbins=50)
 
 ###Layout
 layout = html.Div(
@@ -25,11 +30,19 @@ layout = html.Div(
                         id= 'country',
                         placeholder= 'Select a country',
                         options= dropdown_country))),
+        html.Br(),
         dbc.Row(
     [
-    dbc.Col(html.H2('Graph1')),
+    dbc.Col(html.Div(
+    dcc.Graph(id='pie1',
+              figure= fig1
+              ))
+    ),
     dbc.Col(html.H2('Graph2')),
     ]
-        )
+        ),
     ]
 ),
+
+### Callbacks
+
