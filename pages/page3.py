2ada291acc482dd323bf16d84a161724b7005903
Frans Grau <fransgrau01@gmail.com>
2023-02-02 15:27:03 +0100

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -9,6 +9,11 @@ dash.register_page(__name__,name = 'Price Prediction')
 
 layout = html.Div(
     [
-        dbc.Row(dbc.Col(html.Div(html.H3(["Price Prediction"])))),
+        dbc.Row(dbc.Col(html.Div(html.H3(["Price Overview"])))),
+        dbc.Row([
+                dbc.Col(html.Div(html.H5(["High-end Categories"]))),
+                dbc.Col(html.Div(html.H5(["Most Expensive Regions"]))),
+                ]),
+        
     ]
 )
