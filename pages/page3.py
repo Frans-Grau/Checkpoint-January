88a3956b699e95003915bad55a58015d82d56566
Frans Grau <fransgrau01@gmail.com>
2023-02-02 15:54:40 +0100

    NLTK

--- pages/page3.py
+++ pages/page3.py
@@ -7,6 +7,8 @@ import plotly.express as px
 ### Link
 dash.register_page(__name__,name = 'Price Prediction')
 
+### Upload Dataset
+wine_p = pd.read_csv('https://raw.githubusercontent.com/Frans-Grau/Checkpoint-January/main/ML-Output/wines_with_price.csv')
 layout = html.Div(
     [
         dbc.Row(dbc.Col(html.Div(html.H3(["Price Overview"])))),
